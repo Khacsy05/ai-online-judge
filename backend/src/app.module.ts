3ca2032cdb01a3addd8517b085eb4ea45151a7a5
Khacsy05 <khacsy0@gmail.com>
2026-08-24 02:07:49 +0700

    setup redis bullmq

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,21 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AssignmentsModule } from './modules/assignments/assignments.module';
 import { SubmissionsModule } from './modules/submissions/submissions.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [PrismaModule, AssignmentsModule, SubmissionsModule],
+  imports: [
+    PrismaModule,
+    AssignmentsModule,
+    SubmissionsModule,
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/modules/submissions/submissions.module.ts
+++ backend/src/modules/submissions/submissions.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { SubmissionsService } from './submissions.service';
 import { SubmissionsController } from './submissions.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { BullModule } from '@nestjs/bullmq';
+import { SubmissionsProcessor } from './submissions.processor';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    BullModule.registerQueue({
+      name: 'judging',
+    }),
+  ],
   controllers: [SubmissionsController],
-  providers: [SubmissionsService],
+  providers: [SubmissionsService, SubmissionsProcessor],
   exports: [SubmissionsService],
 })
-export class SubmissionsModule {}
+export class SubmissionsModule { }
